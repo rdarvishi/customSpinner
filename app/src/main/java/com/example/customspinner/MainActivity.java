b576863623be3b045a5075a018002978ef6c466e
rdarvishi <43842614+rdarvishi@users.noreply.github.com>
2020-02-22 12:22:43 +0330

    Initial commit

---
+++ app/src/main/java/com/example/customspinner/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.customspinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner citySpinner;
+    Context context;
+    ArrayList<PersonBean> personBeans;
+    ArrayList<HashMap<String,ArrayList<PersonBean>>> data;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = this;
+        setContentView(R.layout.activity_main);
+
+        citySpinner=findViewById(R.id.cityspinner);
+        preparedData();
+        PersonAdapter adapter=new PersonAdapter(personBeans);
+        citySpinner.setAdapter(adapter);
+    }
+    private void preparedData() {
+        personBeans=new ArrayList<>();
+        personBeans.add(new PersonBean("Status Video","Download Love Song,Sad Song Videos",R.drawable.statusvideologo));
+        personBeans.add(new PersonBean("Status Saver Pro","Download Love Song,Sad Song Videos",R.drawable.statussaverlogo));
+        personBeans.add(new PersonBean("Ramayan By Ramanand Sagar","Download Love Song,Sad Song Videos",R.drawable.ramayanlogo));
+        personBeans.add(new PersonBean("Mahabharat By B.R.Chopra","Download Love Song,Sad Song Videos",R.drawable.mahabharatlogo));
+
+
+
+    }
+}
